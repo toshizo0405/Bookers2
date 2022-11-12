d17d5152d4438d44d39c8e7d0939cc31dc78260e
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-11-12 13:52:21 +0000

    [Update20]4-25

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-   has_one_attached :image  
-   belongs_to :user
    
+   belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          has_many :books, dependent: :destroy
+         has_one_attached :profile_image
 end
