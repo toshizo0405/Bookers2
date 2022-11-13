d32ce0b30a640202b63c37c4c56ba26e977c39ce
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-11-13 07:15:59 +0000

    [Update21]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
          has_many :books, dependent: :destroy
          has_one_attached :profile_image
 end
