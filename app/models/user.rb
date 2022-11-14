dcdaecd4b7737ff8178804009be9cc6a8e6f1466
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-11-14 16:56:58 +0000

    [Update24]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
          has_many :books, dependent: :destroy
          has_one_attached :profile_image
-end
+         
+         def get_image
+          unless image.attached?
+            file_path = Rails.root.join('app/assets/images/default-image.jpg')
+            image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+          end
+          image
+          end
+          
+  end
+
