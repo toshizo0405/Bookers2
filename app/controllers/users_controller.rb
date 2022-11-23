d9a94f84a0639728eb0038a6c3f5492a97acd114
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-11-23 13:33:06 +0000

    [Update44]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    user= User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def index
