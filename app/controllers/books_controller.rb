484ee142076075d8fa563d285ce10f4a58b9552e
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-11-26 13:26:13 +0000

    [Update48]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,9 +20,13 @@ class BooksController < ApplicationController
   end
 
   def update
-     book= Book.find(params[:id])
-     book.update(book_params)
+     @book= Book.find(params[:id])
+     @book.user_id = current_user.id
+    if @book.update(book_params)
      redirect_to book_path(book.id),notice: "You have updated book successfully."
+    else
+     render :edit
+    end
   end
 
   def create
