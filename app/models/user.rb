7dfe18a8975e10208a1402a1441537ccf3dcae8c
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-11-28 09:11:23 +0000

    [Updatee52]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,18 @@ class User < ApplicationRecord
          has_many :books, dependent: :destroy
          has_one_attached :profile_image
 
-         def get_image
-          unless image.attached?
-            file_path = Rails.root.join('app/assets/images/default-image.jpg')
-            image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-          end
-          image
-         end
+         def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/default-image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+end
 
-    validates :name, presence: true
-    
-    
+
+    validates :name, length: { minimum: 2, maximum: 20 },presence: true,uniqueness: true
+    validates :introduction,length: { maximum: 50 }
+
 
 end
 
