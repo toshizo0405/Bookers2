9927e938a88f87a88c99248e1a197368f3e6114d
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-06-28 12:40:30 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
          has_many :books, dependent: :destroy
          has_one_attached :profile_image
 
-         def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/default-image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  profile_image.variant(resize_to_limit: [width, height]).processed
-end
 
 
     validates :name, length: { minimum: 2, maximum: 20 },presence: true,uniqueness: true
